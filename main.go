3a929b8c26fb550707b54f460445b6216d0f6d93
insane-nomad <daglibdaglib@gmail.com>
2023-01-08 22:11:29 +0300

    rename variables

--- main.go
+++ main.go
@@ -10,19 +10,19 @@ import (
 type task func() error
 type tasks []task
 
-func sendToRun(zadanie tasks, numGorutines, maxErrors int) {
+func sendToRun(task tasks, numGoroutines, maxErrors int) {
 	var wg sync.WaitGroup
 	startFrom, step, errCounter := 0, 0, 0
 
 	ch := make(chan struct{})
-	if numGorutines > len(zadanie) { //если количество горутин больше количества заданий
-		numGorutines = len(zadanie) // делаем количество горутин равное количеству заданий
+	if numGoroutines > len(task) { //если количество горутин больше количества заданий
+		numGoroutines = len(task) // делаем количество горутин равное количеству заданий
 	}
 
-	for startFrom < len(zadanie) {
-		step = numGorutines + startFrom
-		if step > len(zadanie) {
-			step = len(zadanie)
+	for startFrom < len(task) {
+		step = numGoroutines + startFrom
+		if step > len(task) {
+			step = len(task)
 		}
 		{
 			//fmt.Printf("startFrom %v - step %v\n", startFrom, step)
@@ -37,7 +37,7 @@ func sendToRun(zadanie tasks, numGorutines, maxErrors int) {
 					defer wg.Done()
 					return
 				}
-				err := zadanie[i]()
+				err := task[i]()
 				if err != nil {
 					errCounter++
 				}
@@ -49,10 +49,10 @@ func sendToRun(zadanie tasks, numGorutines, maxErrors int) {
 		}
 		fmt.Printf("Количество горутин %v\n", runtime.NumGoroutine())
 		wg.Wait()
-		if step >= len(zadanie) {
+		if step >= len(task) {
 			break
 		}
-		startFrom += numGorutines
+		startFrom += numGoroutines
 	}
 }
 
@@ -66,9 +66,9 @@ func IsClosed(ch <-chan struct{}) bool {
 }
 
 func main() {
-	zadanie := make(tasks, 0, 10)
+	tasks := make(tasks, 0, 10)
 	maxErrors := 4
-	numGourutines := 4
+	numGoroutines := 4
 
 	t1 := func() error { fmt.Println("1"); return errors.New("err 1") }
 	t2 := func() error { fmt.Println("2"); return nil }
@@ -80,7 +80,7 @@ func main() {
 	t8 := func() error { fmt.Println("8"); return nil }
 	t9 := func() error { fmt.Println("9"); return nil }
 
-	zadanie = append(zadanie, t1, t2, t3, t4, t5, t6, t7, t8, t9)
-	sendToRun(zadanie, numGourutines, maxErrors)
+	tasks = append(tasks, t1, t2, t3, t4, t5, t6, t7, t8, t9)
+	sendToRun(tasks, numGoroutines, maxErrors)
 
 }
